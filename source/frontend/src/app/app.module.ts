181648b2e7aa64a9d30b2872f6d4aec8eb1459cb
Sergei <46918487+5angar1us@users.noreply.github.com>
2023-08-07 13:16:18 +0300

    Adding frontend from template

---
+++ source/frontend/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {Observable} from 'rxjs';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxBootstrapIconsModule, shield, shieldShaded, shieldFill} from 'ngx-bootstrap-icons';
+
+import {AuthenticationInterceptor} from './services/authentication.interceptor';
+import {AuthenticationService} from './services/authentication.service';
+import {AppComponent} from './app.component';
+import {RequestComponent} from './components/request/request.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+const bootstrapIcons = {
+  shield,
+  shieldShaded,
+  shieldFill,
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    NgxBootstrapIconsModule.pick(bootstrapIcons),
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: keycloakLoaderFactory,
+      deps: [AuthenticationService],
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+export function keycloakLoaderFactory(authenticationService: AuthenticationService): () => Promise<any> | Observable<any> {
+  return () => authenticationService.init();
+}
