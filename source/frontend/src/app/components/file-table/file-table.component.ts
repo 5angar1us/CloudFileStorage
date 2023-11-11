cc656d46a58b18dedcc5a506a88fea8d4979df53
Sergei <46918487+5angar1us@users.noreply.github.com>
2023-11-11 06:36:17 +0300

    adding download file with bad filename

--- source/frontend/src/app/components/file-table/file-table.component.ts
+++ source/frontend/src/app/components/file-table/file-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { S3Object } from 'src/app/Models/S3Object';
 import { faFolder,  faFile } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-file-table',
@@ -11,4 +12,22 @@ export class FileTableComponent {
   @Input() public s3Objects : S3Object[] = []
   faFolder = faFolder;
   faFile = faFile;
+
+  constructor(private http: HttpClient) {}
+
+  donloadFile(path : string, fileName : string){
+    this.http.get('http://localhost:5050/api/v1/File/FileGet',{ observe:'response', responseType: 'blob' , params: { path: path}}).subscribe({
+      next : (respone) => {
+        console.log(respone);
+
+        let blob: Blob = respone.body as Blob;
+
+        let a = document.createElement('a');
+        a.download = fileName
+        a.href = window.URL.createObjectURL(blob);
+        a.click();
+      },
+      error: (e) => console.error(e)
+      });
+  }
 }
